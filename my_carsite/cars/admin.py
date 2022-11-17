5739d9ee9bcda5e177fe4f0cedfb9bbd1802758b
edsonloor <edison.david.loor@gmail.com>
2022-11-16 21:35:11 -0600

    Django forms added

--- my_carsite/cars/admin.py
+++ my_carsite/cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Car
+
 
 # Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('TIME INFORMATION', {'fields': ['year']}),
+        ('CAR INFORMATION', {'fields': ['brand']})
+    ]
+
+
+admin.site.register(Car, CarAdmin)
